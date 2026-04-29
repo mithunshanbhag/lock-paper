a6e6e11ef73472eb79129af2b8da8ed11b8ff0ae
Mithun Shanbhag <mithunshanbhag@users.noreply.github.com>
2026-04-29 20:49:46 +0530

    Initial placeholder MAUI app (#1)

---
+++ src/LockPaper.Ui/Pages/Controls/CategoryChart.xaml.cs
@@ -0,0 +1,10 @@
+namespace LockPaper.Ui.Pages.Controls
+{
+    public partial class CategoryChart
+    {
+        public CategoryChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/LockPaper.Ui/Pages/Controls/ProjectCardView.xaml.cs
@@ -0,0 +1,10 @@
+namespace LockPaper.Ui.Pages.Controls
+{
+    public partial class ProjectCardView
+    {
+        public ProjectCardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
