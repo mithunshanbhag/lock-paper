fc52d02fc4cb93c46c388ceb14eb50be3cf3d0a5
mithun shanbhag <mithunshanbhag@hotmail.com>
2026-05-01 03:22:39 +0530

    feat: Enhance Android MSAL sign-in handling with activity result forwarding and initialization

--- src/LockPaper.Ui/Platforms/Android/MainActivity.cs
+++ src/LockPaper.Ui/Platforms/Android/MainActivity.cs
@@ -1,11 +1,24 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Microsoft.Identity.Client;
 
 namespace LockPaper.Ui
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Microsoft.Maui.ApplicationModel.Platform.Init(this, savedInstanceState);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+        }
     }
 }
--- src/LockPaper.Ui/Platforms/Android/MsalActivity.cs
+++ src/LockPaper.Ui/Platforms/Android/MsalActivity.cs
@@ -1,11 +1,16 @@
-using Android.App;
+﻿using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using LockPaper.Ui.Constants;
 using Microsoft.Identity.Client;
 
 namespace LockPaper.Ui;
 
-[Activity(Exported = true, NoHistory = true)]
+[Activity(
+    Exported = true,
+    LaunchMode = LaunchMode.SingleTask,
+    NoHistory = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
 [IntentFilter(
     [Intent.ActionView],
     Categories = [Intent.CategoryDefault, Intent.CategoryBrowsable],
